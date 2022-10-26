00bb5a45f85c6e3dc6de1f47d9322e041ed39cd6
Maino24 <mg02.gabriele@gmail.com>
2022-10-26 16:33:11 +0200

    creo una variabile somma

--- Snack2/main.js
+++ Snack2/main.js
@@ -5,6 +5,8 @@ Il programma stampa la somma di tutti i numeri inseriti.*/
 //creo un array vuoto 
 const array = []
 
+//creo una variabile somma e la pongo = 0
+let somma = 0
 
 //creo un ciclo for che chiede di inserire 5 numeri
 for(i = 0; i < 5; i++){
@@ -12,7 +14,8 @@ for(i = 0; i < 5; i++){
     let inputNumero = parseInt(prompt(`inserisci un numero`))
     //i numeri li inerisco nell'array 
     array.push(inputNumero)
-
+    somma = somma + inputNumero
 }
 
 console.log(array)
+console.log(somma)

df22f1fb1638166ae13e0155c8045989f1980795
Maino24 <mg02.gabriele@gmail.com>
2022-10-26 16:30:30 +0200

    i numeri li inerisco nell'array

--- Snack2/main.js
+++ Snack2/main.js
@@ -2,13 +2,17 @@
 Il software deve chiedere per 5 volte all’utente di inserire un numero.
 Il programma stampa la somma di tutti i numeri inseriti.*/
 
+//creo un array vuoto 
 const array = []
 
+
+//creo un ciclo for che chiede di inserire 5 numeri
 for(i = 0; i < 5; i++){
 
     let inputNumero = parseInt(prompt(`inserisci un numero`))
+    //i numeri li inerisco nell'array 
     array.push(inputNumero)
 
-
-
 }
+
+console.log(array)

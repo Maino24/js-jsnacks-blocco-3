4e581b12dddea7c92ea386291e6b221cd666332a
Maino24 <mg02.gabriele@gmail.com>
2022-10-26 16:27:10 +0200

    creo un array

---
+++ Snack2/main.js
@@ -0,0 +1,10 @@
+/*
+Il software deve chiedere per 5 volte all’utente di inserire un numero.
+Il programma stampa la somma di tutti i numeri inseriti.*/
+
+const array = []
+
+for(i = 0; i < 5; i++){
+
+    let inputNumero = parseInt(prompt(`inserisci un numero`))
+}

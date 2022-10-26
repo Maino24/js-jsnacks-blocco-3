cc88f4790cc042fdd7e108b98f7d56ef30ff6034
Maino24 <mg02.gabriele@gmail.com>
2022-10-26 22:52:12 +0200

    .

---
+++ Snack3/main.js
@@ -0,0 +1,20 @@
+/*
+Fai inserire un numero, che chiameremo N, all’utente.
+Genera N array,
+ognuno formato da 10 numeri casuali da 1 a 100.
+Ogni volta che ne crei uno, stampalo.*/
+
+
+let n = parseInt(prompt(`inserisci un numero`));
+
+
+for(i = 0; i < n; i++){
+
+
+    let array = math.floor(math.random() *10)
+
+    /*if(){
+
+    }*/
+    console.log(array)
+}
